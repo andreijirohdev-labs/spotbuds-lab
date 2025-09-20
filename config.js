4f6d4e6ce961d077515d342780df52b062e8750d
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2025-09-20 17:42:14 +0800

    chore: use own client ID and update redirect URI

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // production config
-const clientId = "d6f964f061de4aaea35aaa1b5abd1e9d";
-const redirectUri = "https://spotbuds.krishg.com/auth";
+const clientId = "53e6497e3ec3420283c6c35109930674";
+const redirectUri = "https://spotbuds.labs.andreijiroh.dev/auth";
 
 // development config
 // const clientId = "2de5576ababc4bd1a78e213fc729fa62";
